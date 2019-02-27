0db1625a1d91c63107ebb79d776292da9f75eb49
Jason Kim <jasonkim4201@gmail.com>
2019-02-27 10:12:10 -0500

    fixing errors

--- server.js
+++ server.js
@@ -5,33 +5,33 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var MONGODB_URI;
-if(process.env.MONGOLAB_MAROON_URI){
-  MONGODB_URI  = process.env.MONGOLAB_MAROON_URI;
+if (process.env.MONGOLAB_MAROON_URI) {
+  MONGODB_URI = process.env.MONGOLAB_MAROON_URI;
 
-}else{
-  MONGODB_URI  = "mongodb://localhost:27017/financialNews"
+} else {
+  MONGODB_URI = "mongodb://localhost:27017/financialNews"
 }
 var app = express();
 
 app.use(logger("dev"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 // .
-mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 mongoose.connection;
 
 app.get("/scrape", (req, res) => {
   axios.get("https://www.reuters.com/finance/markets").then((response) => {
     var $ = cheerio.load(response.data);
-    const articleArray= [];
+    const articleArray = [];
 
-    $("div.story-content").each(function(i, element) {
-      var result= {};
+    $("div.story-content").each(function (i, element) {
+      var result = {};
 
       result.title = $(this).children().children("h3.story-title").text();
       result.link = $(this).children("a").attr("href");
@@ -41,11 +41,11 @@ app.get("/scrape", (req, res) => {
     });
     console.log(articleArray);
     db.Article.create(articleArray)
-        .then(() => res.send("Article scape has been completed!"))
-        .catch(error => {
-          console.log(error);
-          res.json(error);
-        })
+      .then(() => res.send("Article scape has been completed!"))
+      .catch(error => {
+        console.log(error);
+        res.json(error);
+      })
 
   });
 });
@@ -75,14 +75,14 @@ app.get("/populated", (req, res) => {
 
 // get aritcles by id! and populating with note!
 app.get("/articles/:id", (req, res) => {
-  db.Article.findOne({_id: req.params.id }) 
-  .populate("note")
-  .then((dbArticle) => {
-    res.json(dbArticle);
-  })
-  .catch((error) => {
-    res.json(error);
-  });
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then((dbArticle) => {
+      res.json(dbArticle);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
 });
 
 
@@ -91,7 +91,7 @@ app.get("/articles/:id", (req, res) => {
 app.post("/articles/:id", (req, res) => {
   db.Note.create(req.body)
     .then((dbNote) => {
-      return db.Article.findOneAndUpdate({ _id: req.params.id}, { $push: {note: dbNote._id }}, {new: true});
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { note: dbNote._id } }, { new: true });
     })
     .then((dbArticle) => {
       res.json(dbArticle);
